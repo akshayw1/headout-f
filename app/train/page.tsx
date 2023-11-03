24bedeb02ce7cdfa09ddc20896eb3cc1fd2e29b5
Bhupesh Dewangan <bhupeshdewangan20@gmail.com>
2023-11-04 04:55:07 +0530

    trying-fix

--- app/train/page.tsx
+++ app/train/page.tsx
@@ -2,15 +2,14 @@
 import React from 'react'
 import NavBar from '../components/Navbar/NavBar'
 import WebCam from '../components/WebCam/WebCam'
+import PreviewCam from '../components/WebCam/PreviewCam'
 
 const train = () => {
   return (
     <div>
 <NavBar/>
-
-
 <WebCam/>
-
+<PreviewCam/>
     </div>
   )
 }
