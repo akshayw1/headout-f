8bca3d5312326b41f6718c9b38963a7e7bc19e39
Akshay Waghmare <iec2022117@iiita.ac.in>
2023-11-04 05:26:14 +0530

    chutiya commit bhupi

--- app/train/page.tsx
+++ app/train/page.tsx
@@ -3,13 +3,21 @@ import React from 'react'
 import NavBar from '../components/Navbar/NavBar'
 import WebCam from '../components/WebCam/WebCam'
 import PreviewCam from '../components/WebCam/PreviewCam'
+import FinaLPreview from '../components/WebCam/FinaLPreview'
+import TrainModel from '../components/TrainModel/TrainModel'
 
 const train = () => {
   return (
     <div>
 <NavBar/>
+<div className="mainboard">
 <WebCam/>
-<PreviewCam/>
+<TrainModel/>
+<FinaLPreview/>
+</div>
+
+
+
     </div>
   )
 }
