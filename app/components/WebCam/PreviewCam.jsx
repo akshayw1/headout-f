7f7d14c6b4d9bd2850b61544e338e5347bb9b64d
Bhupesh Dewangan <bhupeshdewangan20@gmail.com>
2023-11-04 03:37:17 +0530

    frame-upload

--- app/components/WebCam/PreviewCam.jsx
+++ app/components/WebCam/PreviewCam.jsx
@@ -4,37 +4,32 @@ import axios from 'axios';
 
 function WebCam() {
   const webcamRef = useRef(null);
-  const [folderName, setFolderName] = useState(''); // Initialize with an empty folder name
 
-  const handleFolderNameChange = (e) => {
-    setFolderName(e.target.value);
-  };
 
-  const capture = async () => {
-
-    const running = setInterval(async () => {
+    setInterval(async () => {
       const imageSrc = webcamRef.current.getScreenshot();
       
       const formData = new FormData();
-      formData.append('folderName', folderName);
       formData.append('image', imageSrc);
-      axios.post('http://localhost:5000/upload', formData, {
+      axios.post('http://localhost:5000/frame', formData, {
         headers: {
           'Content-Type': 'multipart/form-data'
         }
       })
         .then(response => {
-          console.log(response.data);
+          response.json()
         })
+        .then(
+            response => {
+                const result = document.querySelector('.result');
+                result.innerHTML = response;
+            }
+        )
         .catch(error => {
           console.error(error.message);
         });
     }
-    ,100);
-    setTimeout(() => {
-      clearInterval(running)
-    },1000);
-  };
+    ,3000);
 
   return (
     <div className="App">
@@ -43,13 +38,7 @@ function WebCam() {
         ref={webcamRef}
         screenshotFormat="image/jpeg"
       />
-      <input
-        type="text"
-        placeholder="Enter class name"
-        value={folderName}
-        onChange={handleFolderNameChange}
-      />
-      <button onClick={capture}>Start</button>
+      <p className='result'></p>
     </div>
   );
 }
